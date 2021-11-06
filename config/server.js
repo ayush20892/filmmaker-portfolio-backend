5d60ae9c701964fb409b5ef13abff0d4b25aae34
Ayush <ayush2089@gmail.com>
2021-11-06 12:12:59 +0530

    :tada: intial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8e648b8d17e9ca157e8c7fd78792fd20'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fc7b4896-8614-490b-994e-36a9e327c369'
+};
